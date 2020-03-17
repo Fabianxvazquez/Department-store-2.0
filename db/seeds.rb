1bb744b15bee636469a5a44339bf4fa738cbb551
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-17 15:11:18 -0600

    changed a route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Item.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+  )
+end
+
+puts "100 Products Seeded"
+
