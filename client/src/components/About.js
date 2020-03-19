f03e1951162fc80fcec07f32fa562734cf639bc1
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-19 17:20:25 -0600

    changes to about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Header } from "semantic-ui-react";
+// import { Header } from "semantic-ui-react";
 import HeaderText from './HeaderText'
+import styled from 'styled-components';
+
 
 
 const About = () => <HeaderText fSize="small">About</HeaderText>
 
 export default About;
+
+const appContrainer = styled.div`
+  background: linear-gradient (f6f6,white)
+`
--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -103,4 +103,4 @@ const StyledCard = styled(Card)`
   height: 150px;
 `
 
-export default Items;
+
